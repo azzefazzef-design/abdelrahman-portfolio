6df1f6b9ae0938389a8575dd62edb0f0b528375b
عبدالرحمن ثروت <azzefazzef@gmail.com>
2025-10-15 15:58:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // السطر ده هو أهم حاجة في الموضوع كله
+  base: "/abdelrahman-portfolio/", 
   plugins: [react()],
 })
